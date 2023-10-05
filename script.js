374ca38f298ff139788edf48a48674a9c73e9213
Anastasiiasyvak <asyvak@kse.org.ua>
2023-10-05 21:21:18 +0300

    changing status

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 class Task{
     constructor(task){
         this.task = task;
+        this.completed = false;
     }
 }
 
 class All_Tasks{
     constructor(tasks = []){
         this.tasks = tasks;
+        
     }
     addTask(task) {
         this.tasks.push(task);
@@ -18,6 +20,7 @@ class All_Tasks{
             this.tasks.splice(index, 1); 
         }
     }
+
 }
 
 function DisplayTasks(tasks){
@@ -28,7 +31,7 @@ function DisplayTasks(tasks){
         const taskItem = document.createElement("div");
         taskItem.classList.add("task");
         taskItem.innerHTML = `
-            <p>${task.task}</p>
+            <p ${task.completed ? 'style="text-decoration: line-through;"' : ''}>${task.task}</p>
             <button class="button1" onclick="removeTask('${task.task}')">remove this task</button>
             <button class="button2" onclick="changeStatus('${task.task}')">Change status</button>
         `;
@@ -65,14 +68,14 @@ function removeTask(taskText) {
 }
 
 
-function changeStatus(taskText){
+function changeStatus(taskText, button) {
     const taskToChange = shelf.tasks.find(task => task.task === taskText);
-    if (taskToChange){
-        shelf.changeStatus(taskToChange);
+    if (taskToChange) {
+        taskToChange.completed = !taskToChange.completed; 
         DisplayTasks(shelf.tasks);
     }
-
 }
+
 
 const shelf = new All_Tasks([]);
 
